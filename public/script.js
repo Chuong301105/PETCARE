7494fc05f096af89748f887dce72682b62df5278
Chuong301105 <nchdz9999@gmail.com>
2024-09-21 11:31:38 +0700

    Update script.js

--- public/script.js
+++ public/script.js
@@ -10,7 +10,7 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-firebase.initializeApp(firebaseConfig);
+const app = firebase.initializeApp(firebaseConfig);
 const database = firebase.database();
 
 document.addEventListener('DOMContentLoaded', function () {
