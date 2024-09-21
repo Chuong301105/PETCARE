0aba94f15662c736108250e8233551953f738341
Chuong301105 <nchdz9999@gmail.com>
2024-09-21 15:36:17 +0700

    Update script.js

--- public/script.js
+++ public/script.js
@@ -1,17 +1,5 @@
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyD_49CYLkS-4bSAmpHAXPFaKlZ_UmY_46I",
-  authDomain: "petcare-project-dec50.firebaseapp.com",
-  databaseURL: "https://petcare-project-dec50-default-rtdb.firebaseio.com",
-  projectId: "petcare-project-dec50",
-  storageBucket: "petcare-project-dec50.appspot.com",
-  messagingSenderId: "257060539542",
-  appId: "1:257060539542:web:2772a651332c5f155006b7",
-  measurementId: "G-2YFYP2B3Z0"
-};
-
 // Khởi tạo Firebase
-const app = firebase.initializeApp(firebaseConfig);
+firebase.initializeApp(firebaseConfig);
 const database = firebase.database();
 
 // DOM elements
@@ -21,13 +9,13 @@ const dataForm = document.getElementById('dataForm');
 
 // Khi form được submit, ghi dữ liệu vào Firebase
 dataForm.addEventListener('submit', function(event) {
-    event.preventDefault();  // Ngăn chặn việc reload trang
+    event.preventDefault();
 
     // Lấy giá trị từ input
     const message = messageInput.value;
 
-    // Ghi dữ liệu vào Realtime Database
-    firebase.database().ref('messages').push().set({
+    // Ghi dữ liệu vào Firebase Realtime Database
+    firebase.database().ref('messages').push({
         message: message
     });
 
